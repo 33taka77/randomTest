bef685b27192afa2846364cfdac40efd6ba1e14d
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2015-06-19 14:40:39 +0900

    Initial Commit

---
+++ randomTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomTable
+//
+//  Created by Aizawa Takashi on 2015/06/19.
+//  Copyright (c) 2015年 Aizawa Takashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
